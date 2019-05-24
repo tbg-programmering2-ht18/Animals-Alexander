15064767ac98e48f8b2453e78e8974becb3e424f
Alexander Lindholm (DTK) <Alexander.DTK.Lindholm@lystkom.se>
2019-05-24 13:36:53 +0200

    Add project files.

---
+++ AnimalUppgift/Animal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalUppgift
+{
+    class Animal
+    {
+        public string name { get; set; }
+        public string sound { get; set; }
+        public Boolean canFly { get; set; }
+
+        public string Show()
+        {
+            if (canFly == true)
+            {
+                return ("Name: " + name + "\t Sound: " + sound + "\t Can it fly: yes");
+            }
+            else
+            {
+                return ("Name: " + name + "\t Sound: " + sound + "\t Can it fly: No");
+            }
+        }
+    }
+
+    class Dog : Animal
+    {
+        public Dog(string Name)
+        {
+            name = Name;
+            sound = "Arf";
+            canFly = false;
+        }
+
+    }
+
+    class Cat : Animal
+    {
+        public Cat(string Name)
+        {
+            name = Name;
+            sound = "Meow";
+            canFly = false;
+        }
+
+    }
+
+    class Bird : Animal
+    {
+        public Bird(string Name)
+        {
+            name = Name;
+            sound = "Kvitterkvitter";
+            canFly = true;
+        }
+
+    }
+
+}
---
+++ AnimalUppgift/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalUppgift
+{
+    class Program
+    {
+        public static List<Animal> Animals = new List<Animal>();
+        private static readonly Random getrandom = new Random();
+
+        static void Main(string[] args)
+        {
+            //lägger till innehåll för Animals listan
+            Animals.Add(new Dog("Voffe"));
+            Animals.Add(new Cat("Nisse"));
+            Animals.Add(new Bird("VadFanDöperManEnFågel"));
+            Animals.Add(new Dog("Voffe II"));
+
+            Console.WriteLine("God afton \nDu kommer tilldelas ett av fyra djur \nTryck på en knapp för att fortsätta (esc för att avsluta)");
+
+
+
+
+            ConsoleKey key = Console.ReadKey().Key;
+            while (key != ConsoleKey.Escape)
+            {
+                int n = getrandom.Next(0, Animals.Count - 1);
+                Console.WriteLine("\nDitt djur blev:");
+                Console.WriteLine(Animals[n].Show());
+                Console.WriteLine("Tryck på en knapp för att fortsätta (esc för att avsluta): \n");
+                key = Console.ReadKey().Key;
+
+            }
+        }
+    }
+}
